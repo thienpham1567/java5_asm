6c52802f067a198a0418c0bcf1ab2ea1a5ae945a
dtrung112 <85286528+ldt112@users.noreply.github.com>
2023-02-11 15:42:05 +0700

    Update OrderService.java

--- ASM/src/main/java/com/asm/service/OrderService.java
+++ ASM/src/main/java/com/asm/service/OrderService.java
@@ -1,5 +1,51 @@
 package com.asm.service;
 
-public class OrderService {
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.asm.repository.OrderRepository;
+
+import antlr.collections.List;
+import com.asm.entities.DbOrder;
+import com.asm.interfaces.DatabaseService;
+
+
+@Service
+public class OrderService implements DatabaseService<DbOrder>{
+	@Autowired 
+	OrderRepository repo;
+
+	@Override
+	public java.util.List<DbOrder> getAll(boolean isSort) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Optional<DbOrder> findById(int id) {
+		Optional<DbOrder> dbUser = repo.findById(id);
+		return dbUser;
+	}
+
+	@Override
+	public DbOrder create(DbOrder model) {
+		repo.save(model);
+		return model;
+	}
+
+	@Override
+	public DbOrder update(DbOrder model) {
+		repo.save(model);
+		return null;
+	}
+
+	@Override
+	public void delete(int id) {
+		repo.deleteById(id);
+		
+	}
+
+	
 }
